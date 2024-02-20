a2705a2a61615a9a40c7fa360961914e9a29b885
IcePlayer974 <julien.delplanque.jd@gmail.com>
2024-02-20 09:15:37 +0100

    new: executable

--- src/lol/etil2jz/puissance4/Puissance4.java
+++ src/lol/etil2jz/puissance4/Puissance4.java
@@ -1,9 +1,9 @@
 package lol.etil2jz.puissance4;
-
+//Pour lancer le programme dans le src : lol.etil2jz.puissance4.Puissance4
 public class Puissance4 {
 
     public static void main(String[] args) {
-        
+        System.out.println("Hello Word!");
     }
 
 }
