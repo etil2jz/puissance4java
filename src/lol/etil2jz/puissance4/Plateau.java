023650dc60c7d8ef15e10639cc8bfa28bb28df02
IcePlayer974 <julien.delplanque.jd@gmail.com>
2024-02-20 11:24:27 +0100

    color_indicator

--- src/lol/etil2jz/puissance4/Plateau.java
+++ src/lol/etil2jz/puissance4/Plateau.java
@@ -24,28 +24,27 @@ public class Plateau {
 
     public void affichage() {
         // Affichage de la première ligne avec les numéros de colonnes
-        System.out.print("+--");
+        System.out.print("\u001b[36m+--");
         for (int i = 1; i <= longueur; i++) {
             System.out.print(i + "--");
         }
-        System.out.println("+");
-
+        System.out.println("+\u001B[0m");
+    
         // Affichage du reste du plateau avec les séparateurs et les valeurs
         for (int i = 0; i < hauteur; i++) {
-            System.out.print("|  ");
+            System.out.print("\u001B[36m|  \u001B[0m");
             for (int j = 0; j < longueur; j++) {
                 System.out.print(tableau[i][j] + "  ");
             }
-            System.out.println("|");
+            System.out.println("\u001B[36m|\u001B[0m");
         }
-
+    
         // Affichage de la dernière ligne avec les séparateurs
-        System.out.print("+--");
+        System.out.print("\u001b[36m+--");
         for (int i = 1; i <= longueur; i++) {
             System.out.print(i + "--");
         }
-        System.out.println("+");
+        System.out.println("+ \u001B[0m");
     }
+    
 }
-
-
