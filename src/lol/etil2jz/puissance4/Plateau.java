45f6f16b9426512f0e0daa4157fc64a7609d83dd
IcePlayer974 <julien.delplanque.jd@gmail.com>
2024-02-20 11:05:00 +0100

    Crea_tab

--- src/lol/etil2jz/puissance4/Plateau.java
+++ src/lol/etil2jz/puissance4/Plateau.java
@@ -1,28 +1,51 @@
 package lol.etil2jz.puissance4;
 
 public class Plateau {
-    public static void build(){
-        int hauteur = 6; // NB lignes
-        int longeur = 7;//NB colonnes
+    private int hauteur; // Nombre de lignes
+    private int longueur; // Nombre de colonnes
+    private int[][] tableau;
 
-        int[][] tableau = new int[hauteur][longeur];
+    // Constructeur
+    public Plateau(int hauteur, int longueur) {
+        this.hauteur = hauteur;
+        this.longueur = longueur;
+        this.tableau = new int[hauteur][longueur];
+        //initialiserTableau();
     }
-   public static void Affichage(Plateau p){ 
 
-    int hauteur = 6; // NB lignes
-    int longeur = 7;//NB colonnes
-
-    for(int i = 0; i < hauteur; i++){
-        for(int j = 0; j < longeur; j++) {
-            p.tableau[i][j] = i*longeur + j +1;
+    // Méthode pour initialiser le tableau
+   /*  private void initialiserTableau() {
+        for (int i = 0; i < hauteur; i++) {
+            for (int j = 0; j < longueur; j++) {
+                tableau[i][j] = i * longueur + j + 1;
+            }
         }
-    }
+    }*/
 
-    for(int i = 0; i < hauteur; i++){
-        for(int j = 0; j < longeur; j++) {
-            System.out.print(p.tableau[i][j] + "\t");
+    public void affichage() {
+        // Affichage de la première ligne avec les numéros de colonnes
+        System.out.print("+--");
+        for (int i = 1; i <= longueur; i++) {
+            System.out.print(i + "--");
         }
-        System.out.println();
+        System.out.println("+");
+
+        // Affichage du reste du plateau avec les séparateurs et les valeurs
+        for (int i = 0; i < hauteur; i++) {
+            System.out.print("|  ");
+            for (int j = 0; j < longueur; j++) {
+                System.out.print(tableau[i][j] + "  ");
+            }
+            System.out.println("|");
+        }
+
+        // Affichage de la dernière ligne avec les séparateurs
+        System.out.print("+--");
+        for (int i = 1; i <= longueur; i++) {
+            System.out.print(i + "--");
+        }
+        System.out.println("+");
     }
-   }
 }
+
+
--- src/lol/etil2jz/puissance4/Puissance4.java
+++ src/lol/etil2jz/puissance4/Puissance4.java
@@ -1,14 +1,16 @@
 package lol.etil2jz.puissance4;
-//Pour lancer le programme dans le src : lol.etil2jz.puissance4.Puissance4
-//MAIN
+
 public class Puissance4 {
 
     public static void main(String[] args) {
         System.out.println("Hello Word!");
-        
-        Plateau instancPlateau = new Plateau();
 
-        instancPlateau.Affichage();
+        Plateau plateau = new Plateau(6, 7);
+        plateau.affichage();
+
+        // Vous pouvez ajouter d'autres opérations sur le plateau ici si nécessaire
+
+        // Vous pouvez à nouveau afficher le plateau après les modifications
+        //plateau.affichage();
     }
-
 }
